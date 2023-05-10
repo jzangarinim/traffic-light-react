238f4a1d46e077695e8383fbb10a5a13aeef7a22
Jose Zangarini <jzangarinim@gmail.com>
2023-05-10 13:55:11 -0400

    basic structure

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -2,7 +2,13 @@ import React, { useState, useEffect } from "react";
 
 //create your first component
 const Home = () => {
-  return <div className="m-auto bg-dark col-2 mt-5 rounded">o</div>;
+  return (
+    <div className="container m-auto bg-dark col-2 mt-5 rounded pt-5 pb-5 text-white d-flex flex-column align-items-center h-auto">
+      <div className="rounded-circle street-light red-light selected"></div>
+      <div className="rounded-circle street-light yellow-light mt-2"></div>
+      <div className="rounded-circle street-light green-light mt-2"></div>
+    </div>
+  );
 };
 
 export default Home;
