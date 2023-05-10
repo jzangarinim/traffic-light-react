01ac7375aeba8b3665c4ed22c28fe31d9244e948
Jose Zangarini <jzangarinim@gmail.com>
2023-05-10 17:42:14 -0400

    finished

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,13 +1,9 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Light from "./Light.jsx";
 
 //create your first component
 const Home = () => {
   const [active, setActive] = useState(null);
-  const [color, setColor] = useState("");
-  function handleChange() {
-    setColor(event.target.value);
-  }
   return (
     <>
       <div
@@ -17,17 +13,6 @@ const Home = () => {
         <Light color="red" active={active} />
         <Light color="yellow" active={active} />
         <Light color="green" active={active} />
-      </div>
-      <div className="container d-block col-2 mt-3">
-        <label>
-          Choose a color to add:
-          <select className="ms-2 mb-5" value={color} onChange={handleChange}>
-            <option value="" disabled defaultValue=""></option>
-            <option value="purple">Purple</option>
-            <option value="Blue">Blue</option>
-            <option value="Rainbow">Mystery</option>
-          </select>
-        </label>
       </div>
     </>
   );
